5f9dcba261a074e56600f85de00f18a3b7713a30
Quentin Brateau <quentin.brateau@ensta-bretagne.org>
2021-12-01 16:51:43 +0100

    Adding pulsing sphere generation

--- src/image_inversion.py
+++ src/image_inversion.py
@@ -19,11 +19,11 @@ if __name__ == "__main__":
 
     image = np.mean(ac.signal.amplitude_envelope(A[:, :, int(0.25*t_ret.size):int(0.75*t_ret.size)], dt, axis=2), axis=2)
 
-    image = 20 * np.log10(image / a_emitter / 0.5 / 0.5 / 0.0001)
+    image = 20 * np.log10(image / np.max(image))
 
     fig, ax = plt.subplots(1, 1)
     im = ax.imshow(image , cmap="PuBuGn", aspect="equal", interpolation="catrom")
-    ax.set_title("Recieved signal amplitude")
+    ax.set_title("Transmission Loss")
     ax.set_xlabel(r"x ($m$)", fontsize="10")
     ax.set_ylabel(r"y ($m$)", fontsize="10")
     ax.tick_params(axis="both", labelsize="7")
@@ -32,6 +32,6 @@ if __name__ == "__main__":
     divider = make_axes_locatable(ax)
     cax = divider.append_axes("bottom", size="10%", pad=0.8)
     clb = fig.colorbar(im, orientation="horizontal", cax=cax)
-    cax.set_title(r"Ratio $\frac{a_{reciever}}{a_{emitter}}$", fontsize="10")
+    cax.set_title(r"Transmission Loss ($dB$)", fontsize="10")
     cax.tick_params(axis="both", labelsize="7")
     plt.savefig(f"./output/ampltude.png", dpi=180, bbox_inches='tight')
